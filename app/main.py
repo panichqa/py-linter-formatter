6ae1f634439865bcfac09be19794d19c86c55184
Vlada <vlada.belayy@mail.com>
2024-07-18 16:17:27 +0300

    fixed 16-17 and 21-22 lines

--- app/main.py
+++ app/main.py
@@ -12,11 +12,12 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
-        "status": "failed" if [format_linter_error(error)
-                               for error in errors] else "passed"
+        "status": "passed" if not errors else "failed"
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(file_path, errors)
-            for file_path, errors in linter_report.items()]
+    return [
+            format_single_linter_file(file_path, errors)
+            for file_path, errors in linter_report.items()
+    ]
